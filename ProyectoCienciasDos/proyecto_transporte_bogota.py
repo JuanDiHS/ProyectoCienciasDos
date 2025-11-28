4311d54b9e3a184d01cb6d62dc52ee120b613918
JuanDiHS <143758164+JuanDiHS@users.noreply.github.com>
2025-11-28 18:07:47 -0500

    Delete ProyectoCienciasDos directory

--- ProyectoCienciasDos/proyecto_transporte_bogota.py
+++
@@ -1,4 +0,0 @@
-from transport_opt.cli import main_demo
-
-if __name__ == "__main__":
-    main_demo()
--- ProyectoCienciasDos/tests/test_bplustree.py
+++
@@ -1,9 +0,0 @@
-from transport_opt.db.bplustree import BPlusTree
-
-def test_insert_and_search():
-    bpt = BPlusTree(order=4)
-    bpt.insert("A", {"id":"A"})
-    bpt.insert("B", {"id":"B"})
-    assert bpt.search("A")["id"] == "A"
-    assert bpt.search("B")["id"] == "B"
-    assert bpt.search("C") is None
